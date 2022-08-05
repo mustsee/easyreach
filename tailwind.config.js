642a942cac61988e94c1e11a5e69dedcdc52e483
Thomas Sypniewski <thomas.sypniewski@gmail.com>
2022-08-06 00:27:25 +0100

    Try to resolve tailwind colors not showing in prod

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,35 +1,8 @@
-const colors = require("tailwindcss/colors");
+//const colors = require("tailwindcss/colors");
 
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
-  theme: {
-    colors: {
-      transparent: "transparent",
-      current: "currentColor",
-      black: "#000",
-      white: "#fff",
-      amber: colors.amber,
-      gray: colors.gray,
-      red: colors.red,
-      orange: colors.orange,
-      amber: colors.amber,
-      yellow: colors.yellow,
-      lime: colors.lime,
-      green: colors.green,
-      emerald: colors.emerald,
-      teal: colors.teal,
-      cyan: colors.cyan,
-      sky: colors.sky,
-      blue: colors.blue,
-      indigo: colors.indigo,
-      violet: colors.violet,
-      purple: colors.purple,
-      fuchsia: colors.fuchsia,
-      pink: colors.pink,
-      rose: colors.rose,
-    }
-  },
   variants: {
     extend: {},
   },
